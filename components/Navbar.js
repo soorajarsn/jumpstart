162ea0d01d4773993b128b7351319e0678e140b0
Sooraj Shukla <soorajshukla974@gmail.com>
2020-12-14 13:26:51 +0530

    First commit

---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./navbar.css";
+function Navbar() {
+    return (
+        <nav>
+            
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ components/PostCard.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import "./postCard.css";
+import { Heart, HeartFill, Comment } from "./icons";
+function PostCard({ post }) {
+  const [liked, setLiked] = useState(false);
+  const [postDescription, setPostDescription] = useState(post.postDescription);
+  const createMarkup = html => ({ __html: html });
+  useEffect(() => {
+    const hideContent = () => {
+      setPostDescription(() => {
+        console.log("Set Description: ", post.postDescription.substr(0,150)+(post.postDescription.length > 151 && "..."))
+        return post.postDescription.substr(0,150)+(post.postDescription.length > 151 && "...")
+      });
+    }
+    if (window.innerWidth < 700) {
+      hideContent();
+    }
+  }, []);
+  return (
+    <article className="card-container card-font">
+      <div className="about-container flex">
+        <div className="avatar-img-container link">
+          <img src={post.companyImg} alt="" />
+        </div>
+        <div className="author-views-container">
+          <span className="company-name link">{post.companyName}</span>
+          <div className="views-when-container">
+            <i className="fas fa-circle"></i>
+            <span className="views">{post.views}</span>
+          </div>
+        </div>
+      </div>
+      <div className="post-content-container">
+        <span className="post-content-main">
+          <h3 className="post-header">{post.postHeader}</h3>
+          <div className="post-description">
+            {postDescription.split("\n").map((description, index) => (
+              <p key={index} dangerouslySetInnerHTML={createMarkup(description)}></p>
+            ))}
+          </div>
+          {post.postImg && (
+            <div className="post-img">
+              <img src={post.postImg} alt="" />
+            </div>
+          )}
+        </span>
+        <span className="action-buttons-container flex">
+          <button className={"like-button flex align-center link " + (liked && "liked")} onClick={() => setLiked(prev => !prev)}>
+            {(!liked && <Heart />) || <HeartFill />}
+            {post.likesCount != 0 && <span className="likes-count">{post.likesCount + ((liked && 1) || 0)}</span>}
+          </button>
+          <button className="comment-button flex link">
+            <Comment />
+            {post.commentsCount != 0 && <span className="comment-count">{post.commentsCount}</span>}
+          </button>
+        </span>
+      </div>
+    </article>
+  );
+}
+
+export default PostCard;
